8ece17e5360295f1eda54ed7556a37b02f3d1d07
Licia Thörn <liciathorn@gmail.com>
2022-04-07 13:53:20 +0200

    first commit

---
+++ project/app/Http/Controllers/ListController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ListModel;
+use Illuminate\Http\Request;
+
+class ListController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return ListModel::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required', 
+                               
+        ]);
+
+        return ListModel::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return ListModel::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $list = ListModel::find($id);
+        $list->update($request->all());
+        return $list;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+       return ListModel::destroy($id); 
+    }
+
+    /**
+     * Search function
+     *
+     * @param  str  $name
+     * @return \Illuminate\Http\Response
+     */
+    public function search($title)
+    {
+       return ListModel::where('title', 'like', '%'.$title.'%')->get(); 
+    }
+}
---
+++ project/routes/api.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ListController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+//Public routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/lists', [ListController::class, 'index']);
+Route::get('/lists{id}', [ListController::class, 'show']);
+Route::get('/lists/search/{title}', [ListController::class, 'search']);
+
+
+
+//Protected routes
+
+Route::group(['middleware' => ['auth:sanctum']], function (){
+    Route::post('/lists', [ListController::class, 'store']);
+    Route::put('/lists/{id}', [ListController::class, 'update']);
+    Route::delete('/lists/{id}', [ListController::class, 'destroy']);
+    
+    Route::post('/logout', [AuthController::class, 'logout']);
+    
+    
+});
+
+Route::middleware('auth:api')->get('/user', function (Request $request){
+    return $request->user();
+});
